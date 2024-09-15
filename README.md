99b164363452af3d60593392da125daeb392303d
Isaac <124602275+hollezy@users.noreply.github.com>
2024-09-15 22:19:36 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# daily-prophet
+What if... wizards used the internet! Inspired by the Daily Prophet from Harry Potter, made digital. 
+
+My submission for the **Couch to Coder** summer coding camp, hosted by the Bright Network.
---
+++ js/script.js
@@ -0,0 +1,7 @@
+// For Hamburger Menu
+const hamburger = document.getElementById('hamburger');
+const nav = document.getElementById('nav');
+
+hamburger.addEventListener('click', () => {
+    nav.classList.toggle('active');
+});
